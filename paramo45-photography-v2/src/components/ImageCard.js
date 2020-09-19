4a2bb06dca056a8c874907499ed5125522a4bae0
Isaias Garcia-Ariza <isaiasgarciaariza@gmail.com>
2020-09-19 14:55:51 -0700

    images have been resized for better user experience. image gallery is now complete

--- paramo45-photography-v2/src/components/ImageCard.js
+++ paramo45-photography-v2/src/components/ImageCard.js
@@ -2,21 +2,55 @@ import React  from 'react';
 import {SRLWrapper} from 'simple-react-lightbox'
             //beach
 import image1 from '../images/DSC0035.jpg';
-import image2 from '../images/_DSC0037.jpg'
-import image3 from '../images/_DSC0059.jpg'
-import image4 from '../images/_DSC010.jpg'
-import image5 from '../images/DSC_0051.jpg'
-import image6 from '../images/_DSC0130.jpg'
-import image7 from '../images/DSC_0125.jpg'
-import image8 from '../images/DSC_0022.jpg'
-import image9 from '../images/DSC_0044.jpg'
-// import image10 from '../images/_DSC.jpg'
-// import image11 from '../images/_DSC.jpg'
-// import image12 from '../images/_DSC.jpg'
-// import image13 from '../images/_DSC.jpg'
-// import image14 from '../images/_DSC.jpg'
-// import image15 from '../images/_DSC.jpg'
-// import image from '../images/_DSC.jpg'
+import image2 from '../images/_DSC0037.jpg';
+import image3 from '../images/_DSC0059.jpg';
+import image4 from '../images/_DSC010.jpg';
+import image5 from '../images/DSC_0051.jpg';
+import image6 from '../images/_DSC0130.jpg';
+import image7 from '../images/DSC_0125.jpg';
+import image8 from '../images/DSC_0022.jpg';
+import image9 from '../images/DSC_0044.jpg';
+import image10 from '../images/_DSC0036.jpg';
+import image11 from '../images/DSC_0020.jpg';
+import image12 from '../images/_DSC0060.jpg';
+import image13 from '../images/DSC_000467.jpg';
+import image14 from '../images/DSC_000434.jpg';
+import image15 from '../images/DSC_0004.jpg';
+import image16 from '../images/DSC_0478.jpg';
+import image17 from '../images/DSC_5678.jpg';
+import image18 from '../images/puma1.jpg';
+import image19 from '../images/DSC_0105.jpg';
+import image20 from '../images/DSC_0385.jpg';
+import image21 from '../images/_DSC0045.jpg';
+import image22 from '../images/_DSC0058.jpg';
+import image23 from '../images/_DSC0039.jpg';
+import image24 from '../images/DSC_0122.jpg';
+import image25 from '../images/DSC_0053.jpg';
+import image26 from '../images/DSC_0081.jpg';
+import image27 from '../images/DSC_0099.jpg';
+import image28 from '../images/DSC_0280.jpg';
+import image29 from '../images/DSC_035790.jpg';
+import image30 from '../images/DSC_778456.jpg';
+import image31 from '../images/DSC_0070.jpg';
+import image32 from '../images/DSC_0131.jpg';
+import image33 from '../images/DSC_0366.jpg';
+import image34 from '../images/_DSC0051.jpg';
+import image35 from '../images/DSC_0381.jpg';
+import image36 from '../images/DSC_0279.jpg';
+import image37 from '../images/_DSC0116.jpg';
+import image38 from '../images/DSC_0106.jpg';
+import image39 from '../images/DSC_0322.jpg';
+import image40 from '../images/_DSC0141.jpg';
+import image41 from '../images/DSC_0124.jpg';
+import image42 from '../images/_DSC0038.jpg';
+import image43 from '../images/DSC_0506.jpg';
+import image44 from '../images/DSC_0458.jpg';
+import image45 from '../images/DSC_0294.jpg';
+import image46 from '../images/_DSC0038.jpg';
+import image47 from '../images/DSC_0208.jpg';
+import image48 from '../images/DSC_0429.jpg';
+import image49 from '../images/DSC_0064.jpg';
+
 
 const ImageGallery = () => {
 
@@ -50,6 +84,126 @@ const ImageGallery = () => {
           </div>
           <div className='galleryImg img9'>
             <img className='image' src={image9} alt={'restaurant at the pier'}/>
+          </div>
+          <div className='galleryImg img10'>
+            <img className='image' src={image10} alt={'Several silhouettes by the sea'}/>
+          </div>
+          <div className='galleryImg img11'>
+            <img className='image' src={image11} alt={'Hand Statues'}/>
+          </div>
+          <div className='galleryImg img12'>
+            <img className='image' src={image12} alt={'Lights on the freeway'}/>
+          </div>
+          <div className='galleryImg img13'>
+            <img className='image' src={image13} alt={'Domestic pigs'}/>
+          </div>
+          <div className='galleryImg img14'>
+            <img className='image' src={image14} alt={'Scientist with test tube'}/>
+          </div>
+          <div className='galleryImg img15'>
+            <img className='image' src={image15} alt={'River with rocks'}/>
+          </div>
+          <div className='galleryImg img16'>
+            <img className='image' src={image16} alt={'Mexican toy'}/>
+          </div>
+          <div className='galleryImg img17'>
+            <img className='image' src={image17} alt={'Chinese ornament'}/>
+          </div>
+          <div className='galleryImg img18'>
+            <img className='image' src={image18} alt={'Black cat'}/>
+          </div>
+          <div className='galleryImg img19'>
+            <img className='image' src={image19} alt={'Cactus'}/>
+          </div>
+          <div className='galleryImg img20'>
+            <img className='image' src={image20} alt={'Boy gazing at river'}/>
+          </div>
+          <div className='galleryImg img21'>
+            <img className='image' src={image21} alt={'Spanish architecture'}/>
+          </div>
+          <div className='galleryImg img22'>
+            <img className='image' src={image22} alt={'Two women walking'}/>
+          </div>
+          <div className='galleryImg img23'>
+            <img className='image' src={image23} alt={'Solo silhouette at the beach'}/>
+          </div>
+          <div className='galleryImg img24'>
+            <img className='image' src={image24} alt={'Kids at the park'}/>
+          </div>
+          <div className='galleryImg25'>
+            <img className='image' src={image25} alt={'Brown dog'}/>
+          </div>
+          <div className='galleryImg img26'>
+            <img className='image' src={image26} alt={'Bird ornament'}/>
+          </div>
+          <div className='galleryImg img27'>
+            <img className='image' src={image27} alt={'Red flower'}/>
+          </div>
+          <div className='galleryImg img28'>
+            <img className='image' src={image28} alt={'Sunset at the beach'}/>
+          </div>
+          <div className='galleryImg img29'>
+            <img className='image' src={image29} alt={'River'}/>
+          </div>
+          <div className='galleryImg img30'>
+            <img className='image' src={image30} alt={'Cat silhouette'}/>
+          </div>
+          <div className='galleryImg img31'>
+            <img className='image' src={image31} alt={''}/>
+          </div>
+          <div className='galleryImg img32'>
+            <img className='image' src={image32} alt={''}/>
+          </div>
+          <div className='galleryImg img33'>
+            <img className='image' src={image33} alt={''}/>
+          </div>
+          <div className='galleryImg img34'>
+            <img className='image' src={image34} alt={''}/>
+          </div>
+          <div className='galleryImg img35'>
+            <img className='image' src={image35} alt={''}/>
+          </div>
+          <div className='galleryImg img36'>
+            <img className='image' src={image36} alt={''}/>
+          </div>
+          <div className='galleryImg img37'>
+            <img className='image' src={image37} alt={''}/>
+          </div>
+          <div className='galleryImg img38'>
+            <img className='image' src={image38} alt={''}/>
+          </div>
+          <div className='galleryImg img39'>
+            <img className='image' src={image39} alt={''}/>
+          </div>
+          <div className='galleryImg img40'>
+            <img className='image' src={image40} alt={''}/>
+          </div>
+          <div className='galleryImg img41'>
+            <img className='image' src={image41} alt={''}/>
+          </div>
+          <div className='galleryImg img42'>
+            <img className='image' src={image42} alt={''}/>
+          </div>
+          <div className='galleryImg img43'>
+            <img className='image' src={image43} alt={''}/>
+          </div>
+          <div className='galleryImg img44'>
+            <img className='image' src={image44} alt={''}/>
+          </div>
+          <div className='galleryImg img45'>
+            <img className='image' src={image45} alt={''}/>
+          </div>
+          <div className='galleryImg img46'>
+            <img className='image' src={image46} alt={''}/>
+          </div>
+          <div className='galleryImg img47'>
+            <img className='image' src={image47} alt={''}/>
+          </div>
+          <div className='galleryImg img48'>
+            <img className='image' src={image48} alt={''}/>
+          </div>
+          <div className='galleryImg img49'>
+            <img className='image' src={image49} alt={''}/>
           </div>
         </div>
       </SRLWrapper>
